7789479ff016b12616ed1754bf5166a02aaa6432
Cesare Quaranta <cesare.quaranta@pronetics.it>
2017-01-07 00:17:35 +0100

    prima configurazione log

--- src/main/java/wol/server/repository/KryoRepository.java
+++ src/main/java/wol/server/repository/KryoRepository.java
@@ -1,5 +1,7 @@
 package wol.server.repository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -17,7 +19,7 @@ import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
 
 public class KryoRepository<T extends WorldContainer<E,Position>,E extends Entity> {
-
+    final static Logger logger = LoggerFactory.getLogger(KryoRepository.class);
 	private Class<T> wolClass;
 	private File basePath;
 	private Kryo kryo;
@@ -30,17 +32,20 @@ public class KryoRepository<T extends WorldContainer<E,Position>,E extends Entit
 	
 	public Collection<WorldContainer<E,Position>> loadInstances(){
 		Collection<WorldContainer<E,Position>> instances=new ArrayList<WorldContainer<E,Position>>();
-		for(File curFile:basePath.listFiles()){
-			try {
-				if(curFile.isFile() && curFile.canRead()){
-					Input input = new Input(new FileInputStream(curFile));
-					WorldContainer<E,Position> curInstrance=kryo.readObject(input,wolClass);
-					instances.add(curInstrance);
+		if(basePath != null){
+			for(File curFile:basePath.listFiles()){
+				try {
+					if(curFile.isFile() && curFile.canRead()){
+						Input input = new Input(new FileInputStream(curFile));
+						WorldContainer<E,Position> curInstrance=kryo.readObject(input,wolClass);
+						instances.add(curInstrance);
+					}
+				} catch (Exception e) {
+					logger.error("Kryo error for file:"+curFile.getName(), e);
 				}
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
+		}else{
+			logger.error("Invalid Kryo path");
 		}
 		return instances;
 	}
