e61b8bf2fd28d2018fe3d3050d3189e11024120f
cesare.quaranta <cesare.quaranta@pronetics.it>
2017-07-22 22:50:20 +0200

    adeguamenti interfacce

--- src/main/java/edu/wol/server/services/TestInterfeceImpl.java
+++ src/main/java/edu/wol/server/services/TestInterfeceImpl.java
@@ -14,8 +14,8 @@ public class TestInterfeceImpl implements TestInterface {
 	final static Logger logger = LoggerFactory.getLogger(TestInterfeceImpl.class);
 
 	@Override
-	public AsteroidShape generateAsteroidShape() throws IOException, Exception {
-		return AsteroidShapeFactory.getInstance().generateShape();
+	public AsteroidShape generateHidrogenGemShape(float length1,float length2,float length3, float length4,double angoloX,double angolo3X,double angolo1Y,double angolo2Y,double angolo3Y,double angoloZ,float correctXZ,float correctY) throws IOException, Exception {
+		return AsteroidShapeFactory.getInstance().generateHidrogenGemShape(length1,length2,length3,length3,angoloX,angolo3X,angolo1Y,angolo2Y,angolo3Y,angoloZ,correctXZ,correctY);
 	}
 
 
