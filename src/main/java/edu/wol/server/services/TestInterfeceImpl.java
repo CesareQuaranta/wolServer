d3f875003fe2a83a8e8df88602eadea4221c9b0a
cesare.quaranta <cesare.quaranta@pronetics.it>
2017-07-24 21:02:57 +0200

    Introdotto metodo per generare isole

--- src/main/java/edu/wol/server/services/TestInterfeceImpl.java
+++ src/main/java/edu/wol/server/services/TestInterfeceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Component;
 import edu.wol.dom.services.TestInterface;
 import edu.wol.dom.shape.AsteroidShape;
 import edu.wol.dom.shape.AsteroidShapeFactory;
+import edu.wol.dom.shape.PlaneShape;
+import edu.wol.dom.shape.PlanetShapeFactory;
 @Component
 public class TestInterfeceImpl implements TestInterface {
 	final static Logger logger = LoggerFactory.getLogger(TestInterfeceImpl.class);
@@ -16,6 +18,11 @@ public class TestInterfeceImpl implements TestInterface {
 	@Override
 	public AsteroidShape generateHidrogenGemShape(float length1,float length2,float length3, float length4,double angoloX,double angolo3X,double angolo1Y,double angolo2Y,double angolo3Y,double angoloZ,float correctXZ,float correctY) throws IOException, Exception {
 		return AsteroidShapeFactory.getInstance().generateHidrogenGemShape(length1,length2,length3,length3,angoloX,angolo3X,angolo1Y,angolo2Y,angolo3Y,angoloZ,correctXZ,correctY);
+	}
+
+	@Override
+	public PlaneShape generateIsland(int lod, double roughness, double maxHeigth) {
+		return PlanetShapeFactory.getInstance().generateIsland(lod, roughness, maxHeigth);
 	}
 
 
